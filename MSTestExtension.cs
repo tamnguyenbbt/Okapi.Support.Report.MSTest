e4d9e6e5476558e7d9c6ccbf3b8c5de0a12174a3
tamnguyenbbt@gmail.com <tamnguyenbbt@gmail.com>
2019-08-03 21:54:03 +1000

    First commit

---
+++ MSTestExtension.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Okapi.Common.Report;
+using TestResult = Okapi.Report.TestResult;
+
+namespace Okapi.Support.Report.MSTest
+{
+    public static class MSTestExtension
+    {
+        public static OkapiTestContext ToOkapiTestContext(this TestContext testContext)
+        {
+            if (testContext == null)
+            {
+                return null;
+            }
+
+            return new OkapiTestContext
+            {
+                TestName = testContext.TestName,
+                FullyQualifiedTestClassName = testContext.FullyQualifiedTestClassName,
+                TestResult = MSTestOutcomeToTestResult(testContext.CurrentTestOutcome)
+            };
+        }
+
+        private static TestResult MSTestOutcomeToTestResult(UnitTestOutcome testOutcome)
+        {
+            TestResult result = TestResult.Passed;
+
+            switch (testOutcome)
+            {
+                case UnitTestOutcome.Aborted:
+                    result = TestResult.Aborted;
+                    break;
+                case UnitTestOutcome.Error:
+                    result = TestResult.Error;
+                    break;
+                case UnitTestOutcome.Failed:
+                    result = TestResult.Failed;
+                    break;
+                case UnitTestOutcome.Inconclusive:
+                    result = TestResult.Inconclusive;
+                    break;
+                case UnitTestOutcome.InProgress:
+                    result = TestResult.InProgress;
+                    break;
+                case UnitTestOutcome.Passed:
+                    result = TestResult.Passed;
+                    break;
+                case UnitTestOutcome.Timeout:
+                    result = TestResult.Timeout;
+                    break;
+                default:
+                    result = TestResult.Unknown;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Okapi.Common
+
